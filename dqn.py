140746a1991a1074f625e6090ac5ec6eb90eea15
Nil <nil@bureaudigiraat.nl>
2019-10-08 16:06:33 +0200

    Fixed errors, added debug info and episode info

--- dqn.py
+++ dqn.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import torch
 import torch.nn as nn
+from torch.nn.modules import MSELoss
 from torch.optim import SGD
 
 
@@ -11,54 +12,75 @@ class QNetwork(nn.Module):
         super().__init__()
         layers = [nn.Linear(num_input, num_hidden), nn.ReLU(), nn.Linear(num_hidden, num_output)]
         self.network = nn.Sequential(*layers)
+        self.d_type = self.network.state_dict()['0.weight'].type()
 
     def forward(self, x):
         return self.network(x)
 
 
 class DQN(object):
-    def __init__(self, input_size, output_size, optimizer=SGD, lr=0.001, gamma=0.99, epsilon_delta=0.001, epsilon_min=0.05):
+    def __init__(self, input_size, output_size, optimizer=SGD, loss=MSELoss, lr=0.01, gamma=0.99, epsilon_delta=0.0001, epsilon_min=0.05):
         self.input_size = input_size
         self.output_size = output_size
         self.gamma = gamma
         self.epsilon_min = epsilon_min
         self.epsilon_delta = epsilon_delta
         self.epsilon = 1.0
-        self.q_network = QNetwork(input_size, output_size, num_hidden=64)
-        self.target_q_network = QNetwork(input_size, output_size, num_hidden=64)
+        self.q_network = QNetwork(input_size, output_size, num_hidden=32)
+        self.target_q_network = QNetwork(input_size, output_size, num_hidden=32)
         self.update_target_network()
         self.optimizer = optimizer(self.q_network.parameters(), lr=lr)
+        self.loss_function = loss()
 
-    def predict(self, obs, eval=False):
+    def check_obs(self, obs):
+        # Check that obs is a tensor
         if type(obs) == torch.tensor:
             pass
         elif type(obs) == np.ndarray:
             obs = torch.from_numpy(obs)
         else:
             raise ValueError
+        # Check tensor is 2D
         assert len(obs.shape) < 3
         if len(obs.shape) == 1:
             obs = obs.reshape(-1, obs.shape[-1])
+        # Check feature dimension is correct
         assert obs.shape[-1] == self.input_size
+        # Check data type is correct
+        if obs.type() != self.q_network.d_type:
+            obs = obs.type(self.q_network.d_type)
+        return obs
+
+    def predict(self, obs, eval=False):
+        obs = self.check_obs(obs)
 
         q_values = self.q_network(obs)
         if eval:
-            return torch.argmax(q_values)
+            return torch.argmax(q_values).item()
 
         if random.random() < self.epsilon:
-            action = random.randint(self.output_size)
+            action = random.randint(0, self.output_size-1)
         else:
-            action = torch.argmax(q_values)
+            action = torch.argmax(q_values).item()
         return action
 
     def train(self, obses_t, actions, rewards, obses_tp1, dones):
-        predictions = self.q_network(obses_t)[actions]
-        targets = rewards + dones * self.gamma * self.target_q_network(obses_tp1)[actions]
-        loss = predictions - targets
-
+        # Convert to torch.tensor
+        obses_t = self.check_obs(obses_t)
+        actions = torch.from_numpy(actions).to(torch.long)
+        rewards = torch.from_numpy(rewards).to(torch.float32)
+        obses_tp1 = self.check_obs(obses_tp1)
+        dones = torch.from_numpy(dones).to(torch.float32)
+        batch_range = torch.range(0, actions.shape[0]-1, dtype=torch.long)
+        # Calculate loss
         self.optimizer.zero_grad()
-        loss.backwards()
+        predictions = self.q_network(obses_t)[batch_range, actions]
+        targets = rewards + self.gamma * dones * self.target_q_network(obses_tp1)[batch_range, actions]
+        loss = self.loss_function(predictions, targets)
+        # Backprop
+        loss.backward()
         self.optimizer.step()
+        return loss.item()
 
     def update_epsilon(self):
         self.epsilon = max(self.epsilon - self.epsilon_delta, self.epsilon_min)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 from torch.optim import SGD, Adam
+from torch.nn.modules import MSELoss
 import numpy as np
 from exp_replay import ReplayBuffer, PrioritizedReplayBuffer
 from dqn import DQN
@@ -10,39 +11,52 @@ def main(params):
     env = gym.make('MountainCar-v0')
 
     if params['buffer'] == ReplayBuffer:
-        buffer = params['buffer'](params['buffer_size'])
+        buffer = ReplayBuffer(params['buffer_size'])
     elif params['buffer'] == PrioritizedReplayBuffer:
-        buffer = params['buffer'](params['buffer_size'], params['PER_alpha'])
+        buffer = PrioritizedReplayBuffer(params['buffer_size'], params['PER_alpha'])
     else:
         raise ValueError('Buffer type not found.')
 
     if params['algorithm'] == DQN:
-        print(env.action_space)
-        algorithm = params['algorithm'](env.observation_space[0],
-                                        env.action_space.n,
-                                        optimizer=params['optimizer'],
-                                        lr=params['lr'],
-                                        gamma=params['gamma'],
-                                        epsilon_delta=params['epsilon_delta'],
-                                        epsilon_min=params['epsilon_min'])
+        algorithm = DQN(env.observation_space.shape[0],
+                        env.action_space.n,
+                        optimizer=params['optimizer'],
+                        loss=params['loss_function'],
+                        lr=params['lr'],
+                        gamma=params['gamma'],
+                        epsilon_delta=params['epsilon_delta'],
+                        epsilon_min=params['epsilon_min'])
     else:
         raise ValueError('Algorithm type not found.')
-
+    losses = []
+    returns = []
     for i in range(params['episodes']):
         print(i, '/', params['episodes'], end='\r')
         obs_t = env.reset()
-        for t in range(300):
+        t = 0
+        episode_loss = []
+        episode_rewards = []
+        while True:
             env.render()
             action = algorithm.predict(obs_t)
-            obs_tp1, reward, done = env.step(action)
+            t += 1
+            obs_tp1, reward, done, _ = env.step(action)
+            episode_rewards.append(reward)
             buffer.add(obs_t, action, reward, obs_tp1, done)
-            batch = buffer.sample(64)
-            algorithm.train(*batch)
+            if len(buffer) >= params['batch_size']:
+                batch = buffer.sample(params['batch_size'])
+                loss = algorithm.train(*batch)
+                episode_loss.append(loss)
+                algorithm.update_epsilon()
             if done:
                 env.render()
                 print('Episode finished in', t, 'steps')
+                print('Cumm reward:', np.sum(episode_rewards), 'Loss:', np.mean(episode_loss), 'Epsilon:', algorithm.epsilon)
                 break
             obs_t = obs_tp1
+        algorithm.update_target_network()
+        losses.append(np.mean(episode_loss))
+        returns.append(np.sum(episode_rewards))
     env.close()
 
 
@@ -54,9 +68,10 @@ if __name__ == '__main__':
                   'algorithm': DQN,
                   'batch_size': 64,
                   'optimizer': SGD,
-                  'lr': 0.001,
+                  'loss_function': MSELoss,
+                  'lr': 0.01,
                   'gamma': 0.99,
-                  'epsilon_delta': 0.001,
+                  'epsilon_delta': 0.0001,
                   'epsilon_min': 0.05,
                   'target_network_interval': 500,
                   'environment': 'MountainCarContinuous-v0',
